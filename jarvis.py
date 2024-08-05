37b27daa5a1b4aeedafd1d623c1a742812b7431e
Juman <jumanchoudhury498@gmail.com>
2024-08-06 04:02:29 +0530

    project complete

--- jarvis.py
+++ jarvis.py
@@ -1,9 +1,42 @@
 import speech_recognition as sr
 import pyttsx3
+import webbrowser
+import os
+from google.generativeai import generate_text, configure
 
+
+
+
+def aiResponce(prompt):
+        # Set up your API key
+    configure(api_key='AIzaSyDTC0n8nqC2J1qRBKV8V8her84TuV0Dciw')
+    # Generate text
+    response = generate_text(prompt=prompt)
+
+    # Access the result
+    if response and hasattr(response, 'result'):
+        print("Generated Text:")
+        print(response.result)
+        speak(response.result)
+
+
+
+chrome_path = 'C:/Users/juman/AppData/Local/Google/Chrome/Application/chrome.exe' #To get the path of the chorme
+webbrowser.register('chrome', None, webbrowser.BackgroundBrowser(chrome_path))
 def speak(text: str):
     engine.say(text)
     engine.runAndWait()
+
+def proccesData(data):
+     if "open youtube" in data.lower():
+        webbrowser.get('chrome').open('https://www.youtube.com/')
+     elif "play o mahi" in data.lower():
+        webbrowser.get('chrome').open('https://www.youtube.com/watch?v=Etkd-07gnxM')
+     else:
+      aiResponce(data)
+          
+          
+          
 
 if __name__ == "__main__": 
     engine = pyttsx3.init()
@@ -54,6 +87,7 @@ if __name__ == "__main__":
                                     else:
                                         print(f"Command received: {command_text}")
                                         speak(command_text)
+                                        proccesData(command_text)
 
                         except sr.WaitTimeoutError:
                                 print("Listening timed out.")
